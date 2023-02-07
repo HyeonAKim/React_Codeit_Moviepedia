8324fdbbdca7bf234842bd56f6ab57a539b9c7eb
hyeona kim <>
2023-02-07 21:31:46 +0900

    03.sort로 정렬바꾸기

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## React로 영화 리뷰사이트 만들기
+
+## 1. npm init react-app .
+
+## 2. 배열 랜더링해서 json 파일 불러오기
+
+[array].map(([item])=> {return([html]);})
+
+## 3. sort로 정렬바꾸기
+
+const [order, setOrder] = useState("createdAt");
+const sortedItems = items.sort((a, b) => b[order] - a[order]);
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import ReviewList from "./ReviewList";
-import items from '../mock.json'; 
+import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
-  return <div><ReviewList items={items}/></div>
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleRatingClick = () => setOrder("rating");
+  const handleCreatedClick = () => setOrder("createdAt");
+
+  return (
+    <>
+      <div>
+        <button onClick={handleRatingClick}>평점순</button>
+        <button onClick={handleCreatedClick}>최신순</button>
+      </div>
+      <div>
+        <ReviewList items={sortedItems} />
+      </div>
+    </>
+  );
 }
 
 export default App;
