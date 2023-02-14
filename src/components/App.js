b009253d0ba7f432f4aa8c0217cba3561fd2df3a
hyeona kim <foren4012@gmail.com>
2023-02-14 21:01:13 +0900

    07.useEffect로 초기데이터가져오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import ReviewList from "./ReviewList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 
 function App() {
@@ -15,10 +15,15 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
+  const handleLoad = async () => {
     const { reviews } = await getReviews();
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
+
   return (
     <>
       <div>
@@ -27,7 +32,6 @@ function App() {
       </div>
       <div>
         <ReviewList items={sortedItems} onDelete={handleDelete} />
-        <button onClick={handleLoadClick}>불러오기</button>
       </div>
     </>
   );
