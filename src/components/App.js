3ff04f839a50c3dd869dbd46e97de85a54b1e765
hyeona kim <foren4012@gmail.com>
2023-02-25 14:56:59 +0900

    12.네트워크 로딩설정하기

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ function App() {
   const [order, setOrder] = useState("createdAt");
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -20,7 +21,17 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      console.log(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
@@ -46,7 +57,11 @@ function App() {
       </div>
       <div>
         <ReviewList items={sortedItems} onDelete={handleDelete} />
-        {hasNext && <button onClick={handleLoadMore}>더보기</button>}
+        {hasNext && (
+          <button disabled={isLoading} onClick={handleLoadMore}>
+            더보기
+          </button>
+        )}
       </div>
     </>
   );
